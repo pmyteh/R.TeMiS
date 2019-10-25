dd90a761fa38ab0aede3d7e245c712a4bb441f39
Tom Nicholls <github@tomnicholls.me.uk>
2019-10-25 17:06:55 +0100

    Add extra tests

--- tm.plugin.lexisnexis/tests/import.R
+++ tm.plugin.lexisnexis/tests/import.R
@@ -18,5 +18,33 @@ file <- system.file("texts", "lexisnexis_test_copyright_error.html",
                     package = "tm.plugin.lexisnexis")
 corpus <- suppressWarnings(Corpus(LexisNexisSource(file)))
 stopifnot(length(corpus) == 1,
-          corpus[[1]]$meta$id == "resize201812311",
+          corpus[[1]]$meta$id == "TheDaily201812311",
           length(corpus[[1]]$meta$rights) == 0)
+
+# A set of examples of documents with multi-part main texts. That is, the main
+# article text isn't contained within a single top-level <div>, but consists of
+# several top-level <div>, <table>, <ul>, etc. elements.
+file <- system.file("texts", "lexisnexis_test_multipart_main_texts.html",
+                    package = "tm.plugin.lexisnexis")
+corpus <- Corpus(LexisNexisSource(file))
+stopifnot(length(corpus[[1]]$content) == 15)
+
+# Documents with extraneous untagged lines that we *don't* want extracted
+# as part of the main text:
+file <- system.file("texts", "lexisnexis_test_extra_lines.html",
+                    package = "tm.plugin.lexisnexis")
+corpus <- Corpus(LexisNexisSource(file))
+stopifnot(length(corpus[[1]]$content) == 4)
+
+
+# Documents with no heading
+file <- system.file("texts", "lexisnexis_test_no_headings.html",
+                    package = "tm.plugin.lexisnexis")
+corpus <- Corpus(LexisNexisSource(file))
+stopifnot(all(sapply(meta(corpus, "heading"), length) == 0)) # No headings
+
+# Documents with no body content
+file <- system.file("texts", "lexisnexis_test_no_content.html",
+                    package = "tm.plugin.lexisnexis")
+corpus <- Corpus(LexisNexisSource(file))
+stopifnot(all(sapply(corpus, function(x) length(x$content) == 0))) # No body content
